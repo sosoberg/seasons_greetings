0c94a45ee3555220458c0d3bac1f54772aaf3975
Sam Oberg <samueloberg@hotmail.com>
2021-05-08 12:55:43 -0700

    fixed handlebar file pathing and added test handlebar info

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 // Prevent non logged in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const userData = await User.findAll({
       attributes: { exclude: ['password'] },
