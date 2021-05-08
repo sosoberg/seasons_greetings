c538aef17b95ab154d3bc082aa86875059125b61
Shao Ge <geshao3606@gmail.com>
2021-05-07 18:26:30 -0700

    initial api

---
+++ controllers/api/productRoutes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const { Product } = require('../../models');
+// const withAuth = require('../../utils/auth');
+
+router.get('/', async(req, res) => {
+    try {
+    
+    } catch (err) {
+
+    }
+    });
+  
+router.get('/:id', async(req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+    });
+
+
+router.put('/:id', async(req, res) => {
+    try {
+
+    
+    } catch (err) {
+
+    }
+    });
+
+router.post('/', async (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+    });
+  
+router.delete('/:id', async (req, res) => {
+    try {
+        
+    } catch (err) {
+
+    }
+    });
+
+module.exports = router;
