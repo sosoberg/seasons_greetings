8fc5e75d17392ad25f7b14844e59363c4e835c7a
Shao Ge <geshao3606@gmail.com>
2021-05-10 18:47:03 -0700

    product route and withAuth

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,47 +1,51 @@
 const router = require('express').Router();
 const { Product } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth');
 
-router.get('/', async(req, res) => {
+router.get('/', withAuth, async(req, res) => {
     try {
+        const productData = await Product.findAll({
+          ...req.body,
+          user_id: req.session.user_id,
+        });
     
-    } catch (err) {
+        res.status(200).json(productData);
+      } catch (err) {
+        res.status(400).json(err);
+      }
+});
+  
+router.get('/:id', withAuth, async(req, res) => {
+    try {
+        const productData = await Product.findByPk({
+          where: {
+            id: req.params.id,
+            user_id: req.session.user_id,
+          },
+        });
+    
+        if (!productData) {
+          res.status(404).json({ message: 'No project found with this id!' });
+          return;
+        }
+    
+        res.status(200).json(productData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+});
 
-    }
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const projectData = await Product.create({
+          ...req.body,
+          user_id: req.session.user_id,
+        });
+    
+        res.status(200).json(projectData);
+      } catch (err) {
+        res.status(400).json(err);
+      }
     });
   
-router.get('/:id', async(req, res) => {
-    try {
-        
-    } catch (err) {
-
-    }
-    });
-
-
-router.put('/:id', async(req, res) => {
-    try {
-
-    
-    } catch (err) {
-
-    }
-    });
-
-router.post('/', async (req, res) => {
-    try {
-
-    } catch (err) {
-
-    }
-    });
-  
-router.delete('/:id', async (req, res) => {
-    try {
-        
-    } catch (err) {
-
-    }
-    });
-
 module.exports = router;
