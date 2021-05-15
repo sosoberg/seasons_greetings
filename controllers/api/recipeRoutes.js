7b50e9184d34e8da99cfb37c166fb2996d7c7bf2
Shao Ge <geshao3606@gmail.com>
2021-05-15 16:55:05 -0700

    90%

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { Recipe } = require('../../models');
-
 router.get('/', async(req, res) => {
     try {
       const recipeData = await Recipe.findAll();
@@ -9,7 +8,6 @@ router.get('/', async(req, res) => {
       res.status(500).json(err);
     }
   });
-
 router.post('/', async(req, res) => {
    /* req.body
     {
@@ -17,16 +15,25 @@ router.post('/', async(req, res) => {
       ingredients:,
       description: ,
       pictureurl: ,
-      user_id: 
+      user_id:
   */
     try {
-      const recipeData = await Recipe.create(req.body);
+      const recipeData = await Recipe.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
       res.status(200).json(recipeData);
     } catch (err) {
       res.status(400).json(err);
     }
 });
-
-
-
 module.exports = router;
+
+
+
+
+
+
+
+
+
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { User, Recipe } = require('../models');
 const withAuth = require('../utils/auth');
 const { Product } = require('../models');
-
 // Prevent non logged in users from viewing the homepage
 router.get('/', async (req, res) => {
   try {
@@ -10,15 +9,11 @@ router.get('/', async (req, res) => {
       attributes: { exclude: ['password'] },
       order: [['name', 'ASC']],
     });
-
     const users = userData.map((user) => user.get({ plain: true }));
-
     const productData = await Product.findAll({
       order: [['name', 'ASC']],
     });
-
     const products = productData.map((product) => product.get({ plain: true }));
-
     res.render('homepage', {
       users,
       products,
@@ -29,29 +24,27 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
 router.get('/login', (req, res) => {
   // If a session exists, redirect the request to the homepage
   if (req.session.logged_in) {
     res.redirect('/');
     return;
   }
-
   res.render('login');
 });
-
 router.get('/profile', withAuth, async (req, res) => {
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ['password']},
+      include: [{ model: Recipe, attributes: ['id', 'name', 'ingredients', 'description', 'pictureurl','user_id'] }],
     });
-
-    const user = userData.get({ plain: true });
+    let user = userData.get({ plain: true });
+    console.log(user.recipes[0].name)
     res.render('profile', {
       ...user,
+      ...user.recipes,
       logged_in: true
     });
 });
-
 // renders the community page to authorized clients and sends user data
 router.get('/community', withAuth, async (req, res) => {
   const userData = await User.findAll({
@@ -64,9 +57,6 @@ router.get('/community', withAuth, async (req, res) => {
     logged_in: req.session.logged_in,
     user_id: req.session.user_id,
     userName : req.session.username
-
   });
 });
-
 module.exports = router;
-
